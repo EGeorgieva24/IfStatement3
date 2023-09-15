88ce4bb17ea5866be1f9a554a76bf3333909bed6
e.georgieva24 <e.georgieva24@LAB108PC13>
2023-09-15 11:58:14 +0300

    Add project files.

---
+++ IfStatement3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IfStatement3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell us your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >=21)
+            {
+                Console.WriteLine("Congratulations! You are eligible for casting your vote.");
+            }
+            else
+                Console.WriteLine("Sorry,you can't vote :(");
+        }
+    }
+}
